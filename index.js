addc8d7b14ce56020336f7ff928df4239c63a63e
Cole Parsons <cole.parsons@procusus.com>
2020-08-18 17:40:57 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const dotenv = require('dotenv');
 const express = require('express');
 
 const app = express();
-
+const port = process.env.PORT;
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.listen('localhost:8080');
+app.listen(port, () => {console.log(`listening on port ${port}`)});
