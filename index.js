e1b4a954d97c0bc12609defafa6e42a8794965bf
Cole Parsons <cole.parsons@procusus.com>
2020-08-18 18:08:27 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,23 +4,27 @@ const morgan = require('morgan');
 const fs = require('fs');
 const path = require('path');
 
-const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {flags: 'a'});
-const readStream = fs.createReadStream(path.join(__dirname, 'access.log'));
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {flags: 'a'});
 app.use(morgan('combined', {stream: accessLogStream}));
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+  
+  const readStream = fs.createReadStream(path.join(__dirname, 'access.log'));
+
   let streamoutput = '';
 
-  readStream.on('data', (data) => {
+  await readStream.on('data', (data) => {
     streamoutput += data;
+    res.write(streamoutput);
+  }).on('end', () => {
+    console.log('done');
+    res.end('done');
   });
-  readStream.on('end', () => {
-    res.send(streamoutput);
-  });
+
 });
 
 app.listen(port, () => {console.log(`listening on port ${port}`)});
