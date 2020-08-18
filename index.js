538828711678578aa2b7b85f6ab0654698c71b41
Cole Parsons <cole.parsons@procusus.com>
2020-08-18 17:46:35 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const dotenv = require('dotenv');
 const express = require('express');
+const morgan = require('morgan');
+const fs = require('fs');
+const path = require('path');
+
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {flags: 'a'});
 
 const app = express();
 const port = process.env.PORT;
+
+app.use(morgan('combined', {stream: accessLogStream}));
 
 app.get('/', (req, res) => {
   res.send('Hello World');
